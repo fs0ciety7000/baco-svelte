c0711be66554bb5e8fc7f7a9d44df22a52e849b8
fsociety7000 <nd.managment@gmail.com>
2026-02-10 18:15:23 +0100

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// On vérifie si on est sur Coolify via une variable d'env personnalisée
+const isCoolify = process.env.IS_COOLIFY === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// On choisit l'adaptateur selon l'environnement
+		adapter: isCoolify ? adapterNode() : adapterAuto()
 	}
 };
 
